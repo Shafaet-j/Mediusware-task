0016b22b97908927188bef39447bc4a9182b15c9
Shafaet-j <lbshaft01@gmail.com>
2023-09-25 01:38:53 +0600

    problem two modal opening

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+import ModalA from "../Ui/ModalA";
 
 const Problem2 = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+          >
+            All Contacts
+          </button>
+          <ModalA isOpen={isOpen} setIsOpen={setIsOpen} />
+
+          <button className="btn btn-lg btn-outline-warning" type="button">
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
